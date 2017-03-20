5d12e1bcdacdad62e9583733ffcdf1eeeccc9dc0
philMarius <philipcumner@gmail.com>
2017-03-20 17:36:27 +0000

    Trying to fix the fucking capping function

--- Techno.ml
+++ Techno.ml
@@ -54,13 +54,13 @@ let append l1 l2 =
 ;;
 
 (* Remove the right curly braces *)
-let rm_rparen s =
-	Str.global_replace (Str.regexp "{") "" s
+let rm_rparen =
+	Str.global_replace (Str.regexp "{") ""
 ;;
 
 (* Remove the left curly braces *)
-let rm_lparen s =
-	Str.global_replace (Str.regexp "}") "" s
+let rm_lparen =
+	Str.global_replace (Str.regexp "}") ""
 ;;
 
 (* Sort and remove duplicates *)
@@ -72,21 +72,30 @@ let uniq_sort l =
 	in List.sort compare (aux [] sort)
 ;;
 
+let removeDuplicates l1 = 
+	let rec aux l2 = function
+		| [] -> List.rev l2
+		| h :: t -> if List.mem h l2 then aux l2 t else aux (h :: l2) t
+in aux [] l1;;
+
+let sort_uniq l =
+  removeDuplicates( List.sort compare l);;
+
 (* Convert string form of set to list *)
 let str_to_lst str =
 	let rm_paren str =
 		rm_lparen (rm_rparen str)
-	in uniq_sort (Str.split (Str.regexp ",") (rm_paren str))
+	in sort_uniq (Str.split (Str.regexp ",") (rm_paren str))
 ;;
 
 (* Union of two sets *)
 let union l1 l2 =
-	uniq_sort (append l1 l2)
+	sort_uniq (append l1 l2)
 ;;
 
 (* Different union function *)
 let union2 l1 l2 =
-	uniq_sort (List.merge compare l1 l2)
+	sort_uniq (List.merge compare l1 l2)
 ;;
 
 (* Intersection of two sets *)
@@ -107,7 +116,7 @@ let rec intersect l1 l2 =
 
 (* Convert list to string form of set *)
 let lst_to_str l =
-	"{"^String.concat ", " l^"}"
+	"{"^String.concat ", " (sort_uniq l)^"}"
 ;;
 
 (* Remove quotes from the string *)
@@ -190,32 +199,26 @@ let rec typeOf e =
     | TUnion(e1,e2) ->
         (match (typeOf e1), (typeOf e2) with
         | TechnoLang, TechnoLang -> TechnoLang
-        | TechnoLang, TechnoLang -> TechnoMultiLine
         | _ -> raise (TypeError "Input type to union is not of TechnoLang type"))
 		| TIntersection(e1, e2) ->
 				(match (typeOf e1), (typeOf e2) with
 				| TechnoLang, TechnoLang -> TechnoLang
-        | TechnoLang, TechnoLang -> TechnoMultiLine
 				| _ -> raise (TypeError "Input type to intersection is not of TechnoLang type"))
     | TConcat(s1, s2) ->
         (match (typeOf s1), (typeOf s2) with
         | TechnoString, TechnoString -> TechnoString
-        | TechnoString, TechnoString -> TechnoMultiLine
         | _ -> raise (TypeError "Input type to concatenation is not of TechnoString type"))
     | TStrLen(s) ->
         (match typeOf s with
         | TechnoString -> TechnoInt
-        | TechnoString -> TechnoMultiLine
         | _ -> raise (TypeError "Input type to strlen is not of TechnoInt type"))
     | TAppendToList(s,l) ->
         (match (typeOf s), (typeOf l) with
         | TechnoString, TechnoLang -> TechnoLang
-        | TechnoString, TechnoLang -> TechnoMultiLine
         | _ -> raise (TypeError "Input type to appendToList is not of TechnoString and / TechnoString type"))
     | TCap(i, e) ->
         (match (typeOf i), (typeOf e) with
         | TechnoInt, TechnoLang -> TechnoLang
-        | TechnoInt, TechnoLang -> TechnoMultiLine
         | _ -> raise (TypeError "Cannot cap anything not of type TechnoLang"))
     | Eol(e1, e2) ->
         (match (typeOf e1), (typeOf e2) with
@@ -224,7 +227,6 @@ let rec typeOf e =
     | TKleene(l, x, i) ->
         (match (typeOf l), (typeOf x), (typeOf i) with
         | TechnoLang, TechnoString, TechnoInt -> TechnoLang
-        | TechnoMultiLine, TechnoMultiLine, TechnoMultiLine -> TechnoMultiLine
         | _ -> raise (TypeError "Input types to * are not of type TechnoString and TechnoInt"))
     | TConcatABC(i, l) ->
         (match (typeOf i), (typeOf l) with
@@ -262,7 +264,7 @@ let rec eval e =
     | (TAppendToList(TString(s), e2)) -> let e2' = eval e2 in (TAppendToList(TString(s), e2'))
     | (TAppendToList(e1, e2)) -> let e1' = eval e1 in TAppendToList(e1', e2)
 
-    | (TCap(TInt(i), TLang(l))) -> TLang(lst_to_str (limitLength i (str_to_lst l)))
+    | (TCap(TInt(i), TLang(l))) -> TLang(lst_to_str (limit_list_length i (str_to_lst l)))
     | (TCap(TInt(i), e2)) -> let e2' = eval e2 in TCap(TInt(i), e2')
     | (TCap(e1, e2)) -> let e1' = eval e1 in TCap(e1', e2)
 
@@ -290,8 +292,6 @@ let rec evalloop e =
     	raise StuckTerm
 ;;
 
-let evalProg e = evalloop e;; (* <== Starts here *)
-
 let evalProg e =
   match e with
     | Eol(Eol (e1, e2), e3)  -> Eol (evalProg (Eol (e1, e2)), evalProg(e3))
@@ -309,8 +309,8 @@ let rec type_to_string tT =
 let rec print_res res =
   match res with
 		| (TLang x) -> print_string x
+    | Eol( Eol(TLang(l1), TLang(l2)), TLang(l3)) -> print_res (Eol(TLang(l1), TLang(l2))); print_string "\n"; print_res(TLang(l3))
     | Eol(TLang(l1), TLang(l2)) -> print_res (TLang (l1)); print_string "\n"; print_res (TLang(l2))
-    | Eol(Eol(TLang(l1), TLang(l2)), TLang(l3)) -> print_res (Eol(TLang(l1), TLang(l2))); print_string "\n"; print_res(TLang(l3))
     | (TInt n) -> print_int n
     | (TString s) -> print_string s
     | _ -> raise NonBaseTypeResult
